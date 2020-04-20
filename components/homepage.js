9e9001bafa6c69156dd09467921cbcc6ec5edfc8
Joao Cafe <joao.cafe@sky.uk>
2020-04-20 20:31:56 +0100

    extract homepage

---
+++ components/homepage.js
@@ -0,0 +1,36 @@
+import { Fragment } from 'react';
+import { Typography } from 'antd';
+
+import Layout from '../components/layout';
+import { REDDIT_URL } from '../constants';
+
+import Banner from '../components/banner';
+const { Title } = Typography;
+
+const Homepage = () => {
+  const banner = (
+    <Banner>
+      <Fragment>
+        <Title className={'center-align'} level={1}>
+          Ramen! recipes
+        </Title>
+        <Title className={'center-align'} level={3}>
+          A collection of recipes from{' '}
+          <a href={REDDIT_URL} target={'_blank'}>
+            r/ramen
+          </a>
+        </Title>
+      </Fragment>
+    </Banner>
+  );
+
+  return (
+    <Fragment>
+      <Layout title={'Home'} banner={banner}>
+        <Typography className={'homepage'}></Typography>
+      </Layout>
+    </Fragment>
+  );
+};
+
+export default Homepage;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,3 @@
-import { Fragment } from 'react';
-import { Typography } from 'antd';
+import Homepage from '../components/homepage';
 
-import Layout from '../components/layout';
-import { REDDIT_URL } from '../constants';
-
-import Banner from '../components/banner';
-const { Title } = Typography;
-
-export default function Home() {
-  const banner = (
-    <Banner>
-      <Fragment>
-        <Title className={'center-align'} level={1}>
-          Ramen! recipes
-        </Title>
-        <Title className={'center-align'} level={3}>
-          A collection of recipes from{' '}
-          <a href={REDDIT_URL} target={'_blank'}>
-            r/ramen
-          </a>
-        </Title>
-      </Fragment>
-    </Banner>
-  );
-
-  return (
-    <Fragment>
-      <Layout title={'Home'} banner={banner}>
-        <Typography className={'homepage'}></Typography>
-      </Layout>
-    </Fragment>
-  );
-}
+export default Homepage;
