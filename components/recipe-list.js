bb6cba6bb87ebc041220b1e29623bd6c79a383b5
Joao Cafe <joao.cafe@sky.uk>
2020-04-24 09:46:19 +0100

    css for recipe list

--- components/recipe-list.js
+++ components/recipe-list.js
@@ -7,17 +7,19 @@ const { Meta } = Card;
 
 const RecipeList = ({ data }) => {
   return (
-    <Row>
+    <div className={'recipe-list'}>
+    <Row gutter={[40, 40]}>
       {data.map(({ title, slug, image, author }) => (
-        <Col key={title} span={12} md={{ span: 8 }}>
+        <Col key={title} span={24} md={{span: 12}} lg={{ span: 8 }}>
           <Link href={ROUTES.recipes.href} as={slug}>
-            <Card className={'recipe-list__item'} hoverable cover={<img alt={title} src={image} />}>
+            <Card hoverable className={'recipe-list__item'} cover={<div className={'recipe-list__item__img-container'}><img alt={title} src={image} /></div>}>
               <Meta title={title} description={author.name && `u/${author.name}`} />
             </Card>
           </Link>
         </Col>
       ))}
     </Row>
+    </div>
   );
 };
 
