5d0bdaccf1f5a8c7a455d5fbf58b5b0cd842d177
Joao Cafe <joao.cafe@sky.uk>
2020-04-25 23:56:30 +0100

    don't render steps and instructions if no data is present

--- components/instructions.js
+++ components/instructions.js
@@ -1,25 +1,27 @@
 import { Fragment } from 'react';
 import ReactMarkdown from 'react-markdown';
-import { Tabs, Row, Col, Typography } from 'antd';
+import { Tabs, Row, Col } from 'antd';
 import Ingredients from './ingredients';
 import Steps from './steps';
 const { TabPane } = Tabs;
 
-const TabContent = ({ ingredients, steps, description }) => (
+const TabContent = ({ ingredients = [], steps = [], description }) => (
   <Fragment>
     <ReactMarkdown source={description} />
-    <Row gutter={[32, 0]}>
-      <Col span={24} md={{ span: 12 }}>
-        <Ingredients data={ingredients} />
-      </Col>
-      <Col span={24} md={{ span: 12 }}>
-        <Steps data={steps} />
-      </Col>
-    </Row>
+    {!!steps.length && !!ingredients.length && (
+      <Row gutter={[32, 0]}>
+        <Col span={24} md={{ span: 12 }}>
+          <Ingredients data={ingredients} />
+        </Col>
+        <Col span={24} md={{ span: 12 }}>
+          <Steps data={steps} />
+        </Col>
+      </Row>
+    )}
   </Fragment>
 );
 
-const Instructions = ({ components, broth, tare, noodles, oil }) => {
+const Instructions = ({ components }) => {
   return (
     <div className={'instructions'}>
       <Tabs animated={false} defaultActiveKey={0} size={'large'}>
